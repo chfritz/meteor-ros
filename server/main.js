a8d965a80527a14ff9d1846528aa0b8348640606
Christian Fritz <christian.fritz@gmail.com>
2016-04-13 21:48:49 -0700

    Upserting topic updates into Topics collection.

--- server/main.js
+++ server/main.js
@@ -8,13 +8,32 @@ class ROSHandler {
 
     /** list of subscribes, needed to later unsubscribe again */
     this.subscribers = {};
-  };
+
+    /** list of last messages per topic */
+    this.messages = {};
+
+    this.updaters = {};
+  }
+
+  /** regularly take last message for topic and upsert collection */
+  createUpdater(topic, rate) {
+    var self = this;
+    this.updaters[topic] = Meteor.setInterval(function() {
+      Topics.upsert(topic, self.messages[topic]);
+    }, rate * 1000);
+  }
 
   /** subscribe to the given topic of the given message type. The
-   content will be made available in the Topics collection. */
-  subscribe(topic, messageType) {
+      content will be made available in the Topics collection. @param
+      rate: update frequency (Hz)
+  */
+  subscribe(topic, messageType, rate = 1) {
     const self = this;
 
+    // create an update loop for this topic
+    this.createUpdater(topic, rate);
+
+    // subscibe
     ros.types([
       messageType
     ], function(Type) {
@@ -32,9 +51,7 @@ class ROSHandler {
 
       // Subscribes to the topic
       subscriber.subscribe(function(message) {
-        // message.data.should.equal('howdy');
-        console.log(message);
-        Topics.upsert(topic, message);
+        self.messages[topic] = message;
       });
 
       self.subscribers[topic] = subscriber;
@@ -47,9 +64,14 @@ class ROSHandler {
   unsubscribe(topic) {
     const subscriber = this.subscribers[topic];
     if (subscriber) {
-      subscriber.unregisterSubscriber();     
+      subscriber.unregisterSubscriber();
     } else {
       console.log("Not subscribed to ", topic);
+    }
+
+    if (this.updaters[topic]) {
+      Meteor.clearInterval(this.updaters[topic]);
+      delete this.updaters[topic];
     }
   }
 };
